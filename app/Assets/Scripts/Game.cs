45ab8c3fed677d16dbfb33932948e31bc5d9d4f4
GabrieleBonzi <gabri97.gt@gmail.com>
2021-12-03 15:48:03 +0100

    save game progress

--- app/Assets/Scripts/Game.cs
+++ app/Assets/Scripts/Game.cs
@@ -2,43 +2,100 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//public class Game : MonoBehaviour
-//{
-//    public List<Word> words { get; }
-//    public int level { get; set; }
+public class Level
+{
+    public bool played;
+    public bool passed;
+    public float total_points;
 
-//    private void Awake()
-//    {
-//        this.loadWords();
-//    }
+    public Level()
+    {
+        passed = true;
+        played = true;
+        total_points = 0;
+    }
+}
 
 
-//    // Start is called before the first frame update
-//    void Start()
-//    {
-        
-//    }
+public class World
+{
+    public string name;
+    public bool visited;
+    public int current_level;
+    public int max_level;
+    private bool completed;
+    public Level[] levels;
 
-//    // Update is called once per frame
-//    void Update()
-//    {
-        
-//    }
+    public World(int _max_level, Level[] _levels)
+    {
+        visited = false;
+        current_level = 0;
+        max_level = _max_level;
+        completed = false;
+        levels = _levels;
+    }
 
-//    // parse .csv with all words
-//    private void loadWords()
-//    {
-//        var dataset = Resources.Load<TextAsset>("words.csv");
-//        Debug.Log(dataset);
-//        var dataLines = dataset.text.Split('\n');
+    private void UpdateCompleted()
+    {
+        completed = true;
+        foreach (var level in levels)
+        {
+            if(!level.passed){ completed = false; }
+        }
+    }
 
-//        for (int i = 1; i < dataLines.Length; i++) // start from 1 to skip title
-//        {
-//            var data = dataLines[i].Split(',');
-//            if (data[1] == "1")
-//            {
-//                this.words.Add(new Word(data[3]));
-//            }
-//        }
-//    }
-//}
+    public bool Completed()
+    {
+        UpdateCompleted();
+        return completed;
+    }
+}
+
+public class Game : MonoBehaviour
+{
+    static public List<World> worlds;
+    static private string save_filename;
+    static private string config_filename;
+    static private string[] world_names;
+
+    public Game()
+    {
+        save_filename = "SaveGame.json";
+        config_filename = "Config.json";
+        world_names = new string[] { "FirstWorld", "SecondWorld", "ThirdWorld"};
+        worlds = new List<World> { };
+    }
+
+    public void Start()
+    {
+        LoadGameConfig();
+        SaveGameInfo();
+    }
+
+    public void LoadGameConfig()
+    {
+        foreach (var filename in world_names)
+        {
+            var jsonString = System.IO.File.ReadAllText(Application.dataPath + "/Saves/" + filename + ".json");
+            worlds.Add(JsonUtility.FromJson<World>(jsonString));
+        }
+    }
+
+    public void LoadGameInfo()
+    {
+        foreach (var filename in world_names)
+        {
+            var jsonString = System.IO.File.ReadAllText(Application.dataPath + "/Saves/" + filename + "_1.json");
+            worlds.Add(JsonUtility.FromJson<World>(jsonString));
+        }
+    }
+
+    public void SaveGameInfo()
+    {
+        foreach (var world in worlds)
+        {
+            string game_data_string = JsonUtility.ToJson(world);
+            System.IO.File.WriteAllText(Application.dataPath + "/Saves/" + world.name + "_1.json", game_data_string);
+        }
+    }
+}
